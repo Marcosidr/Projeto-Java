93ff979c23e99b8bed0b7a3c5ba60afe605a89b7
Marcos Inácio <marcosincio556@gmail.com>
2025-09-13 09:35:09 -0300

    feat: implementa PilhaHistoricoAtendimentos usando Stack

---
+++ PilhaHistoricoAtendimentos.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class PilhaHistoricoAtendimentos {
+    private Stack<Paciente> historico;
+
+    public PilhaHistoricoAtendimentos() {
+        historico = new Stack<>();
+    }
+
+    public void adicionarAoHistorico(Paciente p) {
+        historico.push(p);
+    }
+
+    public Paciente verUltimoAtendido() {
+        if (!historico.isEmpty()) {
+            return historico.peek();
+        }
+        return null;
+    }
+
+    public void mostrarHistorico() {
+        if (historico.isEmpty()) {
+            System.out.println("Nenhum atendimento realizado ainda.");
+        } else {
+            for (int i = historico.size() - 1; i >= 0; i--) {
+                System.out.println(historico.get(i).exibirInfo());
+            }
+        }
+    }
+}
