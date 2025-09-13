649d0a0aba0fc174c78c207a32137bb4220096fb
Marcos Inácio <marcosincio556@gmail.com>
2025-09-13 09:30:03 -0300

    feat: implementa FilaDeAtendimento usando LinkedList

---
+++ FilaDeAtendimento.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class FilaDeAtendimento {
+    private LinkedList<Paciente> fila;
+
+    public FilaDeAtendimento() {
+        fila = new LinkedList<>();
+    }
+
+    public void adicionarPaciente(Paciente p) {
+        fila.addLast(p);
+    }
+
+    public Paciente atenderPaciente() {
+        if (!fila.isEmpty()) {
+            return fila.removeFirst();
+        }
+        return null;
+    }
+
+    public boolean estaVazia() {
+        return fila.isEmpty();
+    }
+
+    public void mostrarFila() {
+        if (fila.isEmpty()) {
+            System.out.println("Fila de atendimento vazia.");
+        } else {
+            for (Paciente p : fila) {
+                System.out.println(p.exibirInfo());
+            }
+        }
+    }
+}
